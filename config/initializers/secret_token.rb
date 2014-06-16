4d1adea4f58fd128a6e207a9288c26eb47cd84a1
Jesse Shawl <jesse@jshawl.com>
2014-06-15 20:56:49 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gswr::Application.config.secret_key_base = '214fcec4029bb5749e28139fd9f61ed82ad401f04eecf86b2a31c3b32e48bbe1ea2a52b71d9e470f9decd85d279bc9575a2e2722f36de6c7cf6228c0528d314b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gswr::Application.config.session_store :cookie_store, key: '_gswr_session'
